c12cd1ed07c798047ed75df19ae6af5e6ae8d40f
Lennart Mahling <lennart.mahling@lennart-mahling.de>
2026-07-10 16:19:10 +0200

    -Corrected .gitignore file // - Linkage between backend and database within InventoryContext.cs // -Corrected misspelling of InventoryContext.cs and derivates

---
+++ BE_InventoryManagement/InventoryContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+
+namespace BE_InventoryManagement;
+
+using Microsoft.EntityFrameworkCore;
+
+public class InventoryContext : DbContext
+{
+    //Konstruktor, options später in Program.cs
+    public InventoryContext(DbContextOptions<InventoryContext> options) : base(options){}
+
+    public DbSet<Product> Products { get; set; } = default!;
+}
--- BE_InventoryManagement/Product.cs
+++ BE_InventoryManagement/Product.cs
@@ -2,5 +2,11 @@
 
 public class Product
 {
-    
+    public int Id { get; set; }
+    public string ArticleNumber { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    public DateTime ExpirationDate { get; set; }
 }
