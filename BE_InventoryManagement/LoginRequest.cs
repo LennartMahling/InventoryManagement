54f3b756cbe1abe91f064460ae2d54af4dcc84cd
Lennart Mahling <lennart.mahling@lennart-mahling.de>
2026-08-11 23:50:22 +0200

    -Login bugfix test

--- BE_InventoryManagement/LoginRequest.cs
+++ BE_InventoryManagement/LoginRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace BE_InventoryManagement;
+﻿using System.Text.Json.Serialization;
+
+namespace BE_InventoryManagement;
 
 public class LoginRequest
 {
+    [JsonPropertyName("username")]
     public string Username { get; set; } = string.Empty;
+
+    [JsonPropertyName("password")]
     public string Password { get; set; } = string.Empty;
 }
