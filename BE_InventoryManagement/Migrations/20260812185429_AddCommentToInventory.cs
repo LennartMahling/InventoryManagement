105f64da7b3614fa5dab1e5240010815fc7f1bb7
Lennart Mahling <lennart.mahling@lennart-mahling.de>
2026-08-12 20:54:59 +0200

    -feat: add Comment column migration

---
+++ BE_InventoryManagement/Migrations/20260812185429_AddCommentToInventory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BE_InventoryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommentToInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Inventory",
+                type: "TEXT",
+                maxLength: 1000,
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Inventory");
+        }
+    }
+}
